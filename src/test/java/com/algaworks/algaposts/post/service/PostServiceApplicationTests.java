6f9f3c6a01ee33d41799798a670d2751e1bd0d68
raphaelsegallauol <92927444+raphaelsegallauol@users.noreply.github.com>
2025-07-04 11:31:14 -0300

    Criando os 2 projetos

---
+++ src/test/java/com/algaworks/algaposts/post/service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.post.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
